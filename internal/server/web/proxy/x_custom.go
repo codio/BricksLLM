f48455c10d3e22c3e79cd5a55a270128c3178fcb
Sergei Bronnikov <sergeibronnikov87@gmail.com>
2025-02-20 16:53:42 +0000

    wip

--- internal/server/web/proxy/x_custom.go
+++ internal/server/web/proxy/x_custom.go
@@ -66,7 +66,8 @@ func getXCustomHandler(prod bool) gin.HandlerFunc {
 		c.Request.Header.Del("X-Forwarded-For")
 		c.Request.Header.Del("X-Forwarded-Port")
 		c.Request.Header.Del("X-Forwarded-Proto")
-		fmt.Println("=========HEADERS==============")
+		c.Request.Header.Del("Accept")
+		fmt.Println("=========HEADERS2==============")
 		fmt.Println(c.Request.Header)
 		fmt.Println("=======dumpA===========")
 		fmt.Println(string(dumpA))
@@ -83,6 +84,7 @@ func getXCustomHandler(prod bool) gin.HandlerFunc {
 				r.Header.Del("X-Forwarded-For")
 				r.Header.Del("X-Forwarded-Port")
 				r.Header.Del("X-Forwarded-Proto")
+				r.Header.Del("Accept")
 
 				dumpB, _ := httputil.DumpRequest(r, true)
 				fmt.Println("=======dumpB===========")
